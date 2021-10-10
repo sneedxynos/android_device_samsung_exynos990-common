f8b2e99b5d94cd3a189d36170b73438b324049bd
Danny Lin <danny@kdrag0n.dev>
2021-10-10 02:58:47 -0700

    exynos990-common: Switch to Vulkan UI renderer
    We tried doing this on Android 11 and later reverted it due to fd leaks
    during prolonged video calls. However, the issue has been fixed on
    Android 12, so let's enable it again for performance.
    
    Change-Id: I1cd2ca6931aad60f9c48d40aa0212b69c68edef8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,6 +182,9 @@ BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
+# Vulkan
+TARGET_USES_VULKAN := true
+
 # Wi-Fi
 ifneq ($(BOARD_WLAN_DEVICE),qcwcn)
 BOARD_WLAN_BCMDHD_SAE                         := true
