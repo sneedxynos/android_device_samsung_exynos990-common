dc8cbf607b81797f83ef048d1ec3bf7da6c22b40
doomvrr <doomurr@gmail.com>
2023-10-15 01:45:57 +0300

    exynos990-common: decommonize wifi init

--- device-common.mk
+++ device-common.mk
@@ -355,9 +355,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    wifi_brcm.rc \
-    wifi_sec.rc
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
