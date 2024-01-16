696beec31bff953e00a0dd9e6e1b6749b42a91d1
doomvrr <doomurr@gmail.com>
2024-01-16 20:47:09 +0200

    exynos990-common: build android thermal hal 2.0 vendor
    * Camera hal needs it

--- device-common.mk
+++ device-common.mk
@@ -329,6 +329,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor \
     android.hardware.thermal-service.samsung \
     thermal_symlinks.samsung
 
